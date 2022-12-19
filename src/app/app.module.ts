d2b7480b21cefd74435a46304587334b00b99c7f
Harshdeep Bilaiya <harshdeep.bilaiya@nagarro.com>
2022-12-19 16:43:12 +0530

    feat: initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ComponentOneComponent } from './component-one/component-one.component';
+import { ComponentTwoComponent } from './component-two/component-two.component';
+import { ComponentThreeComponent } from './component-three/component-three.component';
+import { CardComponent } from './card/card.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ComponentOneComponent,
+    ComponentTwoComponent,
+    ComponentThreeComponent,
+    CardComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
