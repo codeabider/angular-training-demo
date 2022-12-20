a679e1f015b4c868add55df292e4fa5248f0fb1c
Harshdeep Bilaiya <harshdeep.bilaiya@nagarro.com>
2022-12-20 10:04:57 +0530

    feat: added logger service and created new instance in id service

--- src/app/id.service.ts
+++ src/app/id.service.ts
@@ -1,20 +1,25 @@
+import { LoggerService } from "./logger.service";
+
 let instanceCount = 0;
 
+// TODO: demonstrate error when we try to inject LoggerService w/o decorator
 export class IDService {
     public id: number = 0;
+    loggerService: LoggerService;
 
     constructor() {
         instanceCount++;
-        console.log('service: times instantiated: ', instanceCount);
+        this.loggerService = new LoggerService();
+        this.loggerService.log('service: times instantiated: ', instanceCount);
     }
 
     setID(id: number): void {
-        console.log('service set id: ', id)
+        this.loggerService.log('service set id: ', id)
         this.id = id;
     }
 
     getID(): number {
-        console.log('service get id: ',this.id)
+        this.loggerService.log('service get id: ',this.id)
         return this.id;
     }
 }
---
+++ src/app/logger.service.ts
@@ -0,0 +1,5 @@
+export class LoggerService {
+    log(...content: any): void {
+        console.log(...content);
+    }
+}
